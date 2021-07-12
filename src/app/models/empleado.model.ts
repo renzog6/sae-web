32445f60884c193a93aa6d3437c5960bea7c67d6
Renzo Gorosito <renzog6@gmail.com>
2021-07-12 17:58:54 -0300

    desde Oficina

--- src/app/models/empleado.model.ts
+++ src/app/models/empleado.model.ts
@@ -4,9 +4,9 @@ export class Empleado {
   id: number = 0;
   nombre: string = "";
   apellido: string = "";
-  nacimiento: Date = new Date();
-  dni: string | undefined;
-  cuil: string | undefined;
+  nacimiento!: Date;// = new Date();
+  dni: string = "";
+  cuil: string  = "";
   domicilio: string | undefined;
   contacto: string | undefined;
   categoria: string | undefined;
@@ -15,7 +15,7 @@ export class Empleado {
   estado: any;
   estadoCivil: any;
   info: any;
-  fechaAlta: any;
+  fechaAlta!: Date;
   fechaBaja: any;
 
   constructor() {}
@@ -25,7 +25,13 @@ export class Empleado {
       this.id = dto.idPersona;
       this.nombre = dto.nombre;
       this.apellido = dto.apellido;
+      this.dni = dto.dni;
+      console.log("F. Antes: " + dto.naciento);
       this.nacimiento = new Date(dto.nacimiento);
+      console.log("F. Despues: " + dto.naciento);
+      this.fechaAlta = new Date(dto.fechaAlta);
+      this.fechaBaja = new Date(dto.fechaBaja);
+
     } catch (error) {
       console.error("Log error", error);
     }
@@ -39,6 +45,22 @@ export class Empleado {
       const m = hoy.getMonth() - this.nacimiento.getMonth();
 
       if (m < 0 || (m === 0 && hoy.getDate() < this.nacimiento.getDate())) {
+        edad--;
+      }
+    } catch (error) {
+      console.error("Log error", error);
+    }
+    return edad;
+  }
+
+  getAntiguedad(): number {
+    let edad: number = 0;
+    try {
+      const hoy = new Date();
+      edad = hoy.getFullYear() - this.fechaAlta.getFullYear();
+      const m = hoy.getMonth() - this.fechaAlta.getMonth();
+
+      if (m < 0 || (m === 0 && hoy.getDate() < this.fechaAlta.getDate())) {
         edad--;
       }
     } catch (error) {
